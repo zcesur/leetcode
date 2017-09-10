8dec992b22afb5aa029bc899f28aa1c7946ba043
zcesur <17045339+zcesur@users.noreply.github.com>
2017-09-10 00:33:30 -0400

    Add 51

--- src/SmallestRange.hs
+++ src/SmallestRange.hs
@@ -5,6 +5,11 @@ import Data.Map.Lazy (Map(..))
 
 import Control.Monad.State
 
+-- | You have k lists of sorted integers in ascending order. Find the
+-- smallest range that includes at least one number from each of the k lists.
+--
+-- We define the range [a,b] is smaller than range [c,d] if b-a < d-c or a <
+-- c if b-a == d-c.
 smallestRange :: [[Int]] -> (Int, Int)
 smallestRange xs = evalState (go (0, maxBound :: Int)) xs'
   where
